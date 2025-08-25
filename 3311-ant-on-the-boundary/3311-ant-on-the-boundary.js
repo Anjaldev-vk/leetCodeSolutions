cdedd3bbcb25caed99a2c9c68582f25bffaaaf1e
AnjalDev <159276801+Anjaldev-vk@users.noreply.github.com>
2025-08-25 10:17:31 +0530

    Time: 54 ms (15.63%), Space: 54.5 MB (41.25%) - LeetHub

---
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.js
@@ -0,0 +1,12 @@
+let returnToBoundaryCount = function(nums) {
+    let position=0;
+    let count=0;
+    for (let move of nums) {
+        position += move;
+        if (position === 0) {
+            count++;
+        }
+    }
+
+    return count;
+};
