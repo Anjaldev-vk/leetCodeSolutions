8ae3e68876ed745caf6e0a4078f9ca086b88eb95
AnjalDev <159276801+Anjaldev-vk@users.noreply.github.com>
2025-11-01 16:46:40 +0530

    Time: 52 ms (74.08%), Space: 55.2 MB (14.04%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -4,9 +4,9 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-     return Promise.all([promise1, promise2])
-    .then(([val1, val2]) => val1 + val2);
-    
+    let val1 = await promise1, val2 = await promise2;
+
+    return val1 + val2;
 };
 
 /**
